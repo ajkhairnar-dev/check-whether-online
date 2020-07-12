2da50a886aa1b5e9fd5b288743b1b7ae6472795e
Jayesh Khairnar <50836194+ajkhairnar-dev@users.noreply.github.com>
2020-07-12 23:15:56 +0530

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase : {
-    apiKey: "AIzaSyDbv4R1fD709BG2Dp8KxT_YxaaB5sLLcD8",
+    apiKey: "1fD709BG25sLLcD8",
     authDomain: "check-weather-online.firebaseapp.com",
     databaseURL: "https://check-weather-online.firebaseio.com",
     projectId: "check-weather-online",
     storageBucket: "check-weather-online.appspot.com",
     messagingSenderId: "193149929809",
-    appId: "1:193149929809:web:ef3cc6241ed44a586a2b70",
+    appId: "1:193149929809:web:ef3cc6241ed86a2b70",
     measurementId: "G-SCLT4WX982"
   }
 };
