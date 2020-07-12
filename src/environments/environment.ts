6725ef610ec69e540c082c964100be4505feaccb
Jayesh Khairnar <50836194+ajkhairnar-dev@users.noreply.github.com>
2020-07-12 23:16:44 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
    firebase : {
-    apiKey: "AIzaSyDbv4R1fD709BG2Dp8KxT_YxaaB5sLLcD8",
-    authDomain: "check-weather-online.firebaseapp.com",
+    apiKey: "AIzBG2Dp8KxT_YxaaB5sLLcD8",
+    authDomain: "check-wline.firebaseapp.com",
     databaseURL: "https://check-weather-online.firebaseio.com",
     projectId: "check-weather-online",
     storageBucket: "check-weather-online.appspot.com",
     messagingSenderId: "193149929809",
-    appId: "1:193149929809:web:ef3cc6241ed44a586a2b70",
+    appId: "1:193149929809:web:ef3cc86a2b70",
     measurementId: "G-SCLT4WX982"
   }
 };
